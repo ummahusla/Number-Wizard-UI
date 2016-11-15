d0b355b803af3d14213d3ef0604163c72e96562d
Edvins Antonovs <ummahusla@gmail.com>
2016-11-15 20:21:48 +0000

    Added an empty functions for load level and quit game

--- Assets/LevelManager.cs
+++ Assets/LevelManager.cs
@@ -3,5 +3,12 @@ using System.Collections;
 
 public class LevelManager : MonoBehaviour {
 
+	public void LoadLevel(string name) {
+		Debug.Log("Level load requested for " + name);
+	}
+	
+	public void QuitRequest(string name) {
+		Debug.Log("Quit request for " + name);
+	}
 	
 }
