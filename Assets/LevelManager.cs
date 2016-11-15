58c24f9b407daf724aa223e2840cb43009fcf3cd
Edvins Antonovs <ummahusla@gmail.com>
2016-11-15 20:50:56 +0000

    Added navigation between start and game

--- Assets/LevelManager.cs
+++ Assets/LevelManager.cs
@@ -5,6 +5,7 @@ public class LevelManager : MonoBehaviour {
 
 	public void LoadLevel(string name) {
 		Debug.Log("Level load requested for " + name);
+		Application.LoadLevel(name);
 	}
 	
 	public void QuitRequest(string name) {
